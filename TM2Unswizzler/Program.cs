8de3218ce1037f9ec42053e9a987eaa3887c3a63
Souzooka <bazookaharwell@gmail.com>
2018-09-24 05:25:40 -1000

    Remove GetAddress from Program file

--- TM2Unswizzler/Program.cs
+++ TM2Unswizzler/Program.cs
@@ -13,16 +13,6 @@ namespace TM2Unswizzler
 
     class Program
     {
-        static int GetAddress(int width, int height, int x, int y)
-        {
-            int block = (y & (~0x0f)) * width + (x & (~0x0f)) * 2;
-            int swap = (((y + 2) >> 2) & 0x01) * 4;
-            int line = (((y & (~0x03)) >> 1) + (y & 0x01)) & 0x07;
-            int column = line * width * 2 + ((x + swap) & 0x07) * 4;
-            int offset = ((y >> 1) & 0x01) + ((x >> 2) & 0x02);
-            return block + column + offset;
-        }
-
         static void Main(string[] args)
         {
             // This program works by having a file dragged onto it. If this program is launched without a file arg, exit
